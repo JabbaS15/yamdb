6c0e042fb9afec73f09c314e402803df5bb002f1
Jabba <JabbaS15@yandex.kz>
2022-05-28 00:38:50 +0600

    allowed hosts

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,7 +1,10 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+load_dotenv()
 
 SECRET_KEY = os.getenv(
     'SECRET_KEY',
